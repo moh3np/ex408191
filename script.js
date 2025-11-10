5a9cc7590002eb58f3e13163beb08f101b463539
Mohsen <mohsenpazhoohiyani@gmai.com>
2025-11-10 12:16:09 +0330

    coin trap project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i, "cointrap");
+    } else if (i % 5 === 0) {
+        console.log(i,"trap");
+    } else if (i % 3 === 0) {
+        console.log(i, "coin");
+    }
+}
